5e375cd8865f58d7ec4559cd2e82d194940cd50b
李恩泽 <lienze87@gmail.com>
2024-05-20 09:45:43 +0800

    fix(page): 修改画板页面的多条线段绘制

--- src/pages/board/index.tsx
+++ src/pages/board/index.tsx
@@ -13,7 +13,7 @@ type Point = {
 };
 
 export default function MainApp() {
-  const [lineList, setLineList] = useState<Array<Point>>([]);
+  const [lineList, updateLineList] = useImmer<Array<Point>>([]);
   const [data, updateData] = useImmer({
     status: "drawEnd",
     canvasWidth: 640,
@@ -23,38 +23,8 @@ export default function MainApp() {
   });
 
   const myWhiteBoard = useRef<HTMLDivElement | null>(null);
-  const myCanvas: HTMLCanvasElement = document.createElement("canvas");
-  myCanvas.id = "my-canvas";
+  const myCanvas = useRef<HTMLCanvasElement | null>(null);
   const ctx = useRef<CanvasRenderingContext2D | null>(null);
-
-  useEffect(() => {
-    if (myWhiteBoard.current) {
-      const width = myWhiteBoard.current?.clientWidth || 640;
-      const height = myWhiteBoard.current?.clientHeight || 480;
-      // draft为Proxy对象,会自动计算改变的部分，并创建新对象
-      updateData((draft) => {
-        draft.canvasWidth = width;
-        draft.canvasHeight = height;
-      });
-      myCanvas.width = width;
-      myCanvas.height = height;
-      myWhiteBoard.current.appendChild(myCanvas);
-      ctx.current = myCanvas.getContext("2d");
-
-      handleReset();
-      initHandler(myCanvas);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (!ctx.current) return;
-    ctx.current.fillStyle = data.fillStyle;
-    ctx.current.fillRect(0, 0, data.canvasWidth, data.canvasWidth);
-
-    lineList.forEach((line) => {
-      drawLine(ctx.current, line);
-    });
-  }, [lineList]);
 
   function drawLine(
     ctx: CanvasRenderingContext2D | null,
@@ -74,73 +44,116 @@ export default function MainApp() {
   }
 
   const getMousePosition = (evt: MouseEvent): { x: number; y: number } => {
-    if (!myCanvas) {
+    if (!myCanvas.current) {
       return { x: 0, y: 0 };
     }
     const scrollTop = window.scrollY;
     const position = {
-      x: evt.pageX - myCanvas.getBoundingClientRect().left,
-      y: evt.pageY - myCanvas.getBoundingClientRect().top - scrollTop,
+      x: evt.pageX - myCanvas.current.getBoundingClientRect().left,
+      y: evt.pageY - myCanvas.current.getBoundingClientRect().top - scrollTop,
     };
     return position;
   };
 
-  function initHandler(myCanvas: HTMLCanvasElement) {
-    // Mouse Event Handlers
-    const mouseDraw = {
-      isDown: false,
-      down: { x: 0, y: 0 },
-      current: { x: 0, y: 0 },
-      up: { x: 0, y: 0 },
-      mousedown: function (evt: MouseEvent) {
-        this.isDown = true;
+  // Mouse Event Handlers
+  const mouseDraw = {
+    isDown: false,
+    down: { x: 0, y: 0 },
+    current: { x: 0, y: 0 },
+    up: { x: 0, y: 0 },
+    mousedown: function (evt: MouseEvent) {
+      this.isDown = true;
 
-        this.down = getMousePosition(evt);
-      },
-      mousemove: function (evt: MouseEvent) {
-        if (this.isDown) {
-          this.current = getMousePosition(evt);
-          const line = {
-            key: "move",
-            begin: this.down,
-            end: this.current,
-          };
-
-          setLineList([...lineList.slice(0, -1), line]);
-        }
-      },
-      mouseup: function (evt: MouseEvent) {
-        this.isDown = false;
-
-        this.up = getMousePosition(evt);
-
+      this.down = getMousePosition(evt);
+    },
+    mousemove: function (evt: MouseEvent) {
+      if (this.isDown) {
+        this.current = getMousePosition(evt);
         const line = {
-          key: "end",
+          key: "move",
           begin: this.down,
-          end: this.up,
+          end: this.current,
         };
 
-        setLineList([...lineList, line]);
-        console.log(lineList);
-      },
-      mouseleave: function () {
-        this.isDown = false;
-      },
-    };
+        updateLineList((draft) => {
+          draft.splice(-1, 1, line);
+        });
+      }
+    },
+    mouseup: function (evt: MouseEvent) {
+      this.isDown = false;
 
-    // Mouse Events
-    myCanvas.addEventListener("mousedown", mouseDraw.mousedown, false);
-    myCanvas.addEventListener("mouseup", mouseDraw.mouseup, false);
-    myCanvas.addEventListener("mousemove", mouseDraw.mousemove, false);
-    myCanvas.addEventListener("mouseleave", mouseDraw.mouseleave, false);
-  }
+      this.up = getMousePosition(evt);
+
+      const line = {
+        key: "end",
+        begin: this.down,
+        end: this.up,
+      };
+
+      updateLineList((draft) => {
+        draft.push(line);
+      });
+    },
+    mouseleave: function () {
+      this.isDown = false;
+    },
+  };
+
+  useEffect(() => {
+    if (myWhiteBoard.current) {
+      const width = myWhiteBoard.current.clientWidth || 640;
+      const height = 480;
+
+      // draft为Proxy对象,会自动计算改变的部分，并创建新对象
+      updateData((draft) => {
+        draft.canvasWidth = width;
+        draft.canvasHeight = height;
+      });
+
+      if (!myCanvas.current) return;
+      const canvas = myCanvas.current;
+
+      canvas.width = width;
+      canvas.height = height;
+
+      ctx.current = canvas.getContext("2d");
+
+      handleReset();
+
+      // Mouse Events
+      canvas.addEventListener("mousedown", mouseDraw.mousedown, false);
+      canvas.addEventListener("mouseup", mouseDraw.mouseup, false);
+      canvas.addEventListener("mousemove", mouseDraw.mousemove, false);
+      canvas.addEventListener("mouseleave", mouseDraw.mouseleave, false);
+
+      return () => {
+        canvas.removeEventListener("mousedown", mouseDraw.mousedown, false);
+        canvas.removeEventListener("mouseup", mouseDraw.mouseup, false);
+        canvas.removeEventListener("mousemove", mouseDraw.mousemove, false);
+        canvas.removeEventListener("mouseleave", mouseDraw.mouseleave, false);
+      };
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!ctx.current) return;
+    ctx.current.fillStyle = data.fillStyle;
+    ctx.current.fillRect(0, 0, data.canvasWidth, data.canvasWidth);
+
+    lineList.forEach((line) => {
+      if (line.key !== "end") {
+        drawLine(ctx.current, line);
+      }
+    });
+  }, [lineList]);
 
   const handleReset = () => {
     if (!ctx.current) return;
     ctx.current.fillStyle = data.fillStyle;
     ctx.current.strokeStyle = data.strokeStyle;
     ctx.current.fillRect(0, 0, data.canvasWidth, data.canvasHeight);
-    setLineList([]);
+    updateLineList(() => []);
   };
 
   return (
@@ -152,7 +165,9 @@ export default function MainApp() {
             重置
           </Button>
         </div>
-        <div className="white-board-content" ref={myWhiteBoard}></div>
+        <div className="white-board-content" ref={myWhiteBoard}>
+          <canvas id="my-canvas" ref={myCanvas} />
+        </div>
       </div>
     </div>
   );
